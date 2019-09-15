e0d6cd38a8a81a75b698a4edb6206c0d85930280
CB93 <caleb_1993_@hotmail.com>
2019-09-15 03:01:47 -0700

    Ability to parse and render pokemon names from Pokedex component

--- Frontend/src/Pokedex.js
+++ Frontend/src/Pokedex.js
@@ -1,47 +1,56 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Pokecard from './Pokecard';
 
 class Pokedex extends Component {
-	
+	constructor(props) {
+		super(props);
+			this.state = {
+				pokedexData : [],
+				isLoaded : false
+			} 
+	  }
+
+
 	componentDidMount() {
     axios.get("http://127.0.0.1:4000/")
-      .then(res => {
-        console.log(res)
-	  })
+	  .then(
+		  (result) => {
+			this.setState({
+			pokedexData : result,
+            isLoaded : true
+		  });
+		})
 	}
 
-	static defaultProps = {
-    	pokemon : [
-        	{ id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-        	{ id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-        	{ id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-        	{ id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-        	{ id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-        	{ id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-        	{ id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-        	{ id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
-        ]
-    };
+    shouldComponentUpdate(nextProps, nextState) {
+		return (this.state.pokedexData !== nextState.Pokedex)
+	}
+
 
 	render() {
-    	let hand1 = [];
-    	let hand2 = [...this.props.pokemon];
-    
-    while(hand1.length < hand2.length) {
-        let randomNum = Math.floor(Math.random() * hand2.length)
-        let randomPokemon = hand2.splice(randomNum,1)[0];
-        hand1.push(randomPokemon);
-    }
+		let {isLoaded, pokedexData} = this.state;
 
-    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience,0 )
-    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience,0 )
-
-    return ( 
-			<div>
-      			{exp1}
-    		</div>
-	)
-  }
+		if (!isLoaded) {
+			return ( 
+				<div>
+					<h1>Loading</h1>
+      			</div>
+			)
+		} else {
+			return ( 
+				<div>
+					{pokedexData.data.map(ui => {
+						return 	<Pokecard 
+									key={ui._id} 
+									name={ui.Name}
+									generation={ui.Generation}/>
+						})
+					}
+				</div>
+			)
+		}
+  	}
 }
 
 
